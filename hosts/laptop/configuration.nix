1e05cf503b3c921f177fc1d9a9fa4fef60cf6d6d
Tim Hämisch <github@thaemisch.net>
2025-05-06 17:32:43 +0200

    laptop kmonad config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -23,11 +23,11 @@
     enable = true;
     keyboards = {
       mzKMonadOutput = {
-        device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+        device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
         config = builtins.readFile ../../dots/kmonad/laptopV2.kbd;
-      }
-    }
-  }
+      };
+    };
+  };
   ############################################
   # BOOTLOADER ###############################
   boot.loader.systemd-boot.enable = true;
